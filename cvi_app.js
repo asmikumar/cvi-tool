494397835249b4c55faccf2a72816d339bf4fea4
asmikumar <asmikumar.ak@gmail.com>
2020-07-09 15:46:01 -0400

    Adding direct specification capabilities

--- cvi_app.js
+++ cvi_app.js
@@ -1,4 +1,7 @@
-var speedModifier = 0.125;
+var input_speed = 125; // low = 50, medium = 125, high = 200
+var speedModifier = 0.001*input_speed;
+
+// --------------------------------------------------------------------
 
 $(document).ready(function(){
     animateDiv();
@@ -7,8 +10,8 @@ $(document).ready(function(){
 function makeNewPosition(){
     
     // Get viewport dimensions (remove the dimension of the div)
-    var h = $(window).height() - sliderSize.value;
-    var w = $(window).width() - sliderSize.value;
+    var h = $(window).height() - $('#anim_ball').height();
+    var w = $(window).width() - $('#anim_ball').width();
     
     var nh = Math.floor(Math.random() * h);
     var nw = Math.floor(Math.random() * w);
@@ -38,119 +41,3 @@ function calcSpeed(prev, next) {
     return speed;
 
 }
-
-var sliderSpeed = document.getElementById("rangeSpeed");
-var sliderSize = document.getElementById('rangeSize');
-var output = document.getElementById("anim_ball");
-
-$("#full").spectrum({
-    color: "#FF5722",
-    showInput: true,
-    className: "full-spectrum",
-    showInitial: true,
-    showPalette: true,
-    showSelectionPalette: true,
-    maxSelectionSize: 10,
-    preferredFormat: "hex",
-    localStorageKey: "spectrum.demo",
-    move: function (color) {
-        
-    },
-    show: function () {
-    
-    },
-    beforeShow: function() {
-    },
-    hide: function () {
-    
-    },
-    change: function(color) {
-    var output = document.getElementById("anim_ball");
-    output.style.setProperty("background-color", color.toHexString());
-    },
-    palette: [
-        ["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)",
-        "rgb(204, 204, 204)", "rgb(217, 217, 217)","rgb(255, 255, 255)"],
-        ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
-        "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"], 
-        ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", 
-        "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)", 
-        "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", 
-        "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)", 
-        "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", 
-        "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
-        "rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
-        "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
-        "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", 
-        "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
-    ]
-});
-
-$("#full2").spectrum({
-    color: "#FFFFFF",
-    showInput: true,
-    className: "full-spectrum",
-    showInitial: true,
-    showPalette: true,
-    showSelectionPalette: true,
-    maxSelectionSize: 10,
-    preferredFormat: "hex",
-    localStorageKey: "spectrum.demo",
-    move: function (color) {
-        
-    },
-    show: function () {
-    
-    },
-    beforeShow: function() {
-    },
-    hide: function () {
-    
-    },
-    change: function(color) {
-    
-    document.body.style.setProperty("background-color", color.toHexString());
-    },
-    palette: [
-        ["rgb(0, 0, 0)", "rgb(67, 67, 67)", "rgb(102, 102, 102)",
-        "rgb(204, 204, 204)", "rgb(217, 217, 217)","rgb(255, 255, 255)"],
-        ["rgb(152, 0, 0)", "rgb(255, 0, 0)", "rgb(255, 153, 0)", "rgb(255, 255, 0)", "rgb(0, 255, 0)",
-        "rgb(0, 255, 255)", "rgb(74, 134, 232)", "rgb(0, 0, 255)", "rgb(153, 0, 255)", "rgb(255, 0, 255)"], 
-        ["rgb(230, 184, 175)", "rgb(244, 204, 204)", "rgb(252, 229, 205)", "rgb(255, 242, 204)", "rgb(217, 234, 211)", 
-        "rgb(208, 224, 227)", "rgb(201, 218, 248)", "rgb(207, 226, 243)", "rgb(217, 210, 233)", "rgb(234, 209, 220)", 
-        "rgb(221, 126, 107)", "rgb(234, 153, 153)", "rgb(249, 203, 156)", "rgb(255, 229, 153)", "rgb(182, 215, 168)", 
-        "rgb(162, 196, 201)", "rgb(164, 194, 244)", "rgb(159, 197, 232)", "rgb(180, 167, 214)", "rgb(213, 166, 189)", 
-        "rgb(204, 65, 37)", "rgb(224, 102, 102)", "rgb(246, 178, 107)", "rgb(255, 217, 102)", "rgb(147, 196, 125)", 
-        "rgb(118, 165, 175)", "rgb(109, 158, 235)", "rgb(111, 168, 220)", "rgb(142, 124, 195)", "rgb(194, 123, 160)",
-        "rgb(166, 28, 0)", "rgb(204, 0, 0)", "rgb(230, 145, 56)", "rgb(241, 194, 50)", "rgb(106, 168, 79)",
-        "rgb(69, 129, 142)", "rgb(60, 120, 216)", "rgb(61, 133, 198)", "rgb(103, 78, 167)", "rgb(166, 77, 121)",
-        "rgb(91, 15, 0)", "rgb(102, 0, 0)", "rgb(120, 63, 4)", "rgb(127, 96, 0)", "rgb(39, 78, 19)", 
-        "rgb(12, 52, 61)", "rgb(28, 69, 135)", "rgb(7, 55, 99)", "rgb(32, 18, 77)", "rgb(76, 17, 48)"]
-    ]
-});
-
-// Update the current slider SPEED value (each time you drag the slider handle)
-sliderSpeed.oninput = function() {
-speedModifier = 0.001*sliderSpeed.value;
-console.log(speedModifier);
-}
-
-// Update the current slider SIZE value (each time you drag the slider handle)
-sliderSize.oninput = function(){
-    document.getElementById("anim_ball").style.height = sliderSize.value.toString().concat("px");
-    document.getElementById("anim_ball").style.width = sliderSize.value.toString().concat("px");
-}
-// -----------------------------------------------------------------------------------
-function changeIt(img) {
-    var location = img.src;
-    $(this).css("border","5px solid plum");
-    document.getElementById("anim_ball").src = location.toString();
-}
-
-// select a shape from array
-$(document).on("click","#figures img",function(){
-    $(this).css("border","5px solid plum");
-    $("#figures img").not($(this)).css("border","5px solid transparent");
-    var location = this.src;
-    document.getElementById("anim_ball").src = location.toString();
-});
