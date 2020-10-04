db048318fb3530deb992803e726036d4e90510be
asmikumar <asmikumar.ak@gmail.com>
2020-10-04 02:17:51 -0400

    Editing gaze tracking

--- static/webgazer.js
+++ static/webgazer.js
@@ -45442,9 +45442,9 @@ function store_points(x, y, k) {
      * @returns {*}
      */
     webgazer.begin = function(onFail) {
-        if (window.location.protocol !== 'https:' && window.location.hostname !== 'localhost' && window.chrome){
-            alert("WebGazer works only over https. If you are doing local development you need to run a local server.");
-        }
+        // if (window.location.protocol !== 'https:' && window.location.hostname !== 'localhost' && window.chrome){
+        //     alert("WebGazer works only over https. If you are doing local development you need to run a local server.");
+        // }
 
         // Load model data stored in localforage.
         if (window.saveDataAcrossSessions) {
